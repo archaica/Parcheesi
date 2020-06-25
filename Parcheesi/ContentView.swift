755f94f42bd400cf41f21ec3686d35e7e1d42bd6
Benjamin Munson <ben@munson.land>
2020-06-24 20:22:57 -0700

    Initial Commit

---
+++ Parcheesi/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Parcheesi
+//
+//  Created by Ben Munson on 6/24/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
